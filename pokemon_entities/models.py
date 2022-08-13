a4f0964c398e817942c98f482250eba66e29b9a9
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-08-13 11:35:36 +0300

    rename related_name for pokemon evolutions

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -24,7 +24,7 @@ class Pokemon(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='evolutions',
+        related_name='next_evolutions',
         verbose_name='Из кого эволюционировал'
     )
     image = models.ImageField(
